da8223e7c6346640b37ad896ac1a29c8c7f9093b
SebasCepedaTobon <sebasct2204@gmail.com>
2021-11-05 01:52:10 -0500

    Se realizan los estilos de los CARDS y se vinculan el resto de etiquetas faltantes

--- 5) CARDS/app.js
+++ 5) CARDS/app.js
@@ -1,4 +1,4 @@
-const main = document.querySelector('#main');
+const tarjeta_wrap = document.querySelector('.tarjeta-wrap');
 
 
 create_card();
@@ -7,19 +7,30 @@ function create_card(){
     const card=document.createElement('div');
     const titleCard = document.createElement('h2');
     const imgCard = document.createElement('img');
+    const preCard = document.createElement('p');
+    const card2=document.createElement('div');
     const pCard = document.createElement('p');
-    const btnCard = document.createElement('button');
-    card.classList.add('card');
-    titleCard.textContent="Hola";
-    imgCard.setAttribute('src', 'fondo3.jpg')
+    const imgCarrito = document.createElement('img');
+
+    card.classList.add('tarjeta');
+    titleCard.textContent="MAUSE";
+    preCard.textContent="100.000";
+    preCard.setAttribute('id', 'precio');
+    imgCard.setAttribute('id', 'articulo')
+    imgCard.setAttribute('src', 'img/fondo3.jpg')
+    card2.classList.add('atras')
     pCard.textContent="Este es el carrito";
-    btnCard.textContent="Agregar al carrito";
+    pCard.setAttribute('id', 'info');
+    imgCarrito.setAttribute('src', 'img/carrito.png');
+    imgCarrito.setAttribute('id', 'carrito');
 
     card.appendChild(titleCard);
     card.appendChild(imgCard);
-    card.appendChild(pCard);
-    card.appendChild(btnCard);
-    main.appendChild(card);
+    card.appendChild(preCard);
+    card.appendChild(card2);
+    card2.appendChild(pCard);
+    card2.appendChild(imgCarrito);
+    tarjeta_wrap.appendChild(card);
 }
 
 function recorrer(cantidad){
@@ -29,7 +40,7 @@ function recorrer(cantidad){
     }
 }
 
-recorrer(50);
+recorrer(10);
 
 
 
