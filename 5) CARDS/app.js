5171705887d8c70f869aad4931e4dac891f725a1
SebasCepedaTobon <sebasct2204@gmail.com>
2021-11-16 23:20:54 -0500

    Se modifican algunos estilos y se agrega el header al HTML

--- 5) CARDS/app.js
+++ 5) CARDS/app.js
@@ -1,36 +1,34 @@
-const tarjeta_wrap = document.querySelector('.tarjeta-wrap');
+const main = document.querySelector('#main');
+const cardTarjeta= document.querySelector('#tarjeta-wrap')
 
-
-create_card();
 
 function create_card(){
     const card=document.createElement('div');
-    const titleCard = document.createElement('h2');
     const imgCard = document.createElement('img');
     const preCard = document.createElement('p');
     const card2=document.createElement('div');
     const pCard = document.createElement('p');
     const imgCarrito = document.createElement('img');
+    const boton = document.createElement('button');
 
     card.classList.add('tarjeta');
-    titleCard.textContent="MAUSE";
     preCard.textContent="100.000";
     preCard.setAttribute('id', 'precio');
     imgCard.setAttribute('id', 'articulo')
-    imgCard.setAttribute('src', 'img/fondo3.jpg')
+    imgCard.setAttribute('src', 'img/imagen2.png')
     card2.classList.add('atras')
-    pCard.textContent="Este es el carrito";
+    pCard.textContent="Leonel Andres Messi juagador del Paris, Media punta con una media de 94";
     pCard.setAttribute('id', 'info');
     imgCarrito.setAttribute('src', 'img/carrito.png');
     imgCarrito.setAttribute('id', 'carrito');
 
-    card.appendChild(titleCard);
+    cardTarjeta.appendChild(card);
     card.appendChild(imgCard);
     card.appendChild(preCard);
     card.appendChild(card2);
     card2.appendChild(pCard);
     card2.appendChild(imgCarrito);
-    tarjeta_wrap.appendChild(card);
+    main.appendChild(cardTarjeta);
 }
 
 function recorrer(cantidad){
@@ -41,6 +39,10 @@ function recorrer(cantidad){
 }
 
 recorrer(10);
+
+
+
+
 
 
 
