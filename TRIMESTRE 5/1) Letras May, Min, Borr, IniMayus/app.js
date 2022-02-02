420f6d13df92f46e7eef96f7dc3cde6f2f014df2
SebasCepedaTobon <sebasct2204@gmail.com>
2022-02-01 22:41:47 -0500

    Avance del ejercicio 1, 5 trimestre

--- TRIMESTRE 5/1) Letras May, Min, Borr, IniMayus/app.js
+++ TRIMESTRE 5/1) Letras May, Min, Borr, IniMayus/app.js
@@ -1,26 +1,41 @@
 const textArea = document.querySelector('.textArea')
 const botonMayuscula = document.querySelector('.mayuscula')
-const botonMinuscula = document.querySelector('.miniscula')
+const botonMinuscula = document.querySelector('.minuscula')
 const botonIniMayuscula = document.querySelector('.IMayuscula')
 const botonBorrar = document.querySelector('.borrar')
 
-botonMayuscula.addEventListener('click', upperCase);
-botonMinuscula.addEventListener('click', lowerCase);
-botonIniMayuscula.addEventListener('click', upperCase);
+botonMayuscula.addEventListener('click', Mayus);
+botonMinuscula.addEventListener('click', minus);
+botonIniMayuscula.addEventListener('click', IniciMayus);
 botonBorrar.addEventListener('click', borra);
 
-function upperCase(){
+console.log(textArea.value)
+
+function Mayus(){
     let text = textArea.value
     text = text.toUpperCase()
     textArea.value = text
     console.log(text)
 }
 
-function lowerCase(){
+function minus(){
     let text = textArea.value
     text = text.toLowerCase()
     textArea.value = text
 }
+
+function IniciMayus(frase){
+    let palabra = frase.split(' ')
+    console.log(palabra)
+
+    return palabra.map(p => p[0].toUpperCase() + p.slice(1)).join(' ');
+
+}
+
+console.log(IniciMayus(textArea.value))
+
+textArea.value = IniciMayus(textArea.value)
+
 
 function borra(){
     textArea.value = ""
